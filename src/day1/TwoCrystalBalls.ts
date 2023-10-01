44d24e8c1e780685a4cf6586ff589a87fc30c871
Mario Bezerra <mariomgsb@gmail.com>
2023-09-30 23:48:54 -0300

    BubbleSort and TwoCrystalBalls problems solved

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let jumpCount = 0;
+
+    for (let i = 0 ; i < breaks.length ; i += jumpAmount){
+        if(breaks[i] == true){
+            break;
+        }
+        jumpCount += jumpAmount;
+    }
+
+    const backOfTheJump = jumpCount - jumpAmount;
+    for(let j = backOfTheJump; j < breaks.length ; j++){
+        if(breaks[j] == true){
+            return j;
+        }
+    }
+
+    return -1;
 }
