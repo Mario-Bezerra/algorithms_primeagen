5fa1734b00a9cbe7f68924457e08ec42e97ebe6b
Mario Bezerra <mariomgsb@gmail.com>
2023-10-01 02:14:51 -0300

    Stack implemented

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+    value : T,
+    previous ?: Node<T>,
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head ?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        const node: Node<T> = { value: item };
+        this.length++;
+        if(!this.head){
+            this.head = node;
+        }
+        const head = this.head;
+        this.head = node;
+        node.previous = head;
+    }
     pop(): T | undefined {
+        if(!this.head){
+            return undefined;
+        }
+        this.length--;
+        const head = this.head;
+        this.head = this.head.previous;
 
-}
+        if(this.length == 0){
+            this.head = undefined;
+        }
+
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
