22a5fd96485e8928140b8576ea81ddf80bc7808b
Mario Bezerra <mariomgsb@gmail.com>
2023-10-07 20:23:55 -0300

    Implementing a Breadth First Search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,30 @@
+import Queue from "@code/Queue";
+
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
+    const queue = new Queue<BinaryNode<number>>;
+    queue.enqueue(head);
+    while(queue.length){
+        const curr : BinaryNode<number> | undefined = queue.peek();
+        
+        if(!curr){
+            break;
+        }
+
+        if(curr.value === needle){
+            return true;
+        }
+
+        if(curr.left){
+            queue.enqueue(curr.left);
+        }
+
+        if(curr.right){
+            queue.enqueue(curr.right);
+        }
+
+        queue.deque();
+    }
+
+    return false;
 }
