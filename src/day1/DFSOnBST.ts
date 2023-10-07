9453f4ed21d7f32eba306b362f5ae312707cdea6
Mario Bezerra <mariomgsb@gmail.com>
2023-10-07 20:25:06 -0300

    Implementing a Depth-First algoritm

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,17 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+export default function dfs(head: BinaryNode<number> | null, needle: number): boolean {
+    let curr : BinaryNode<number> | null= head;
 
+    if(!curr){
+        return false;
+    }
+
+    if(curr.value === needle){
+        return true;
+    }
+
+    if(needle >= curr.value){
+        return dfs(curr.right, needle);
+    }
+
+    return dfs(curr.left, needle);
 }
